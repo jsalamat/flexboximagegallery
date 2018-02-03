954061227156c8ae676b163b2e94a0f783e78512
jsalamat <salamat_jon@yahoo.com>
2018-02-02 23:33:08 -0500

    transitionend is working and toggle open active

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,3 +12,19 @@ panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 // the reason it not toggleOpen() because it would run on page load
 // toggleOpen is just a reference to function 
 // when click to look for function and run
+
+function toggleActive(e) {
+	console.log(e.propertyName)
+	if (e.propertyName.includes('flex')) {
+		this.classList.toggle('open-active');
+	}
+}
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+// listen for transitionend event and toggleActive function
+// this will have multiple transitionend event fired
+// ^^ to figure it out -  put e for event and console log the event
+// when transition finish it will tell what transition
+
+// Summary
+// listen for click on each panel - it will toggle open in css
+// after that finishes it trigger transitionend and then toggle open-active
